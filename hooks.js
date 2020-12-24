eab072b2107f480abe757054d5491e02e8fbd9b2
play.js <git@gituser.com>
2020-12-24 13:44:20 -0800

    try/catch in Hooks

--- hooks.js
+++ hooks.js
@@ -1,16 +1,22 @@
 function Hold()
 {
   //window.alert("in Hold");
-  document.getElementById("debug").innerHTML=" Executing Hold";
+  try
+  {
+    document.getElementById("debug").innerHTML=" Executing Hold";
   
-  let latlon=document.getElementById("latlon").value;
-  window.alert(latlon);
-  let legs=document.getElementById("legs").value;
-  let length=document.getElementById("length").value;
-  let loops=document.getElementById("loops").value;
-  let lat=latlon.split(",")[0];
-  let lon=latlon.split(",")[1];
-  let params="lat="+lat+" lon="+lon+" legs="+legs+" length="+length+" loops="+loops);
-  document.getElementById("result").innerHTML=params;
-  
+    let latlon=document.getElementById("latlon").value;
+    window.alert(latlon);
+    let legs=document.getElementById("legs").value;
+    let length=document.getElementById("length").value;
+    let loops=document.getElementById("loops").value;
+    let lat=latlon.split(",")[0];
+    let lon=latlon.split(",")[1];
+    let params="lat="+lat+" lon="+lon+" legs="+legs+" length="+length+" loops="+loops);
+    document.getElementById("result").innerHTML=params;
+  }
+  catch(err) 
+  {
+    document.getElementById("result").innerHTML = err.message;
+  }
 }
