ea4d658c620163bcb222306db4c56ba7dacc17a0
play.js <git@gituser.com>
2020-12-24 10:27:09 -0800

    more debug

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ var rl = readline.createInterface({
 function Hold()
 {
   window.alert("in Hold");
+  document.getElementById("debug").innerHTML="Executing Hold";
   let latlon=document.getElementById("latlon").value;
   window.alert(latlon);
   let legs=document.getElementById("legs").value;
