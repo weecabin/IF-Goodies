7f658179efbae3be7e4b03cd8e2a39c7e9afd2e8
play.js <git@gituser.com>
2020-12-24 14:20:14 -0800

    011

--- hooks.js
+++ hooks.js
@@ -2,8 +2,8 @@ function Hold()
 {
   //window.alert("in Hold");
     document.getElementById("debug").innerHTML=" Executing Hold";
-    /*
     let latlon=document.getElementById("latlon").value;
+    /*
     window.alert(latlon);
     let legs=document.getElementById("legs").value;
     let length=document.getElementById("length").value;
