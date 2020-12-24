e72c338d40c73fac9485ae3d1f31a06229943826
play.js <git@gituser.com>
2020-12-24 13:02:19 -0800

    moved HTML JavaScript to hooks.js

---
+++ hooks.js
@@ -0,0 +1,16 @@
+function Hold()
+{
+  window.alert("in Hold");
+  window.document.getElementById("debug").innerHTML="Executing Hold";
+  /*
+  let latlon=document.getElementById("latlon").value;
+  window.alert(latlon);
+  let legs=document.getElementById("legs").value;
+  let length=document.getElementById("length").value;
+  let loops=document.getElementById("loops").value;
+  let lat=latlon.split(",")[0];
+  let lon=latlon.split(",")[1];
+  let params="lat="+lat+" lon="+lon+" legs="+legs+" length="+length+" loops="+loops);
+  document.getElementById("result").innerHTML=params;
+  */
+}
--- index.js
+++ index.js
@@ -38,21 +38,6 @@ var rl = readline.createInterface({
   terminal: false
 })
 
-function Hold()
-{
-  window.alert("in Hold");
-  window.document.getElementById("debug").innerHTML="Executing Hold";
-  let latlon=document.getElementById("latlon").value;
-  window.alert(latlon);
-  let legs=document.getElementById("legs").value;
-  let length=document.getElementById("length").value;
-  let loops=document.getElementById("loops").value;
-  let lat=latlon.split(",")[0];
-  let lon=latlon.split(",")[1];
-  let params="lat="+lat+" lon="+lon+" legs="+legs+" length="+length+" loops="+loops);
-  document.getElementById("result").innerHTML=params;
-}
-
 rl.on('line', (line) => 
 {
   var rsp = line.replace(/\0/g, '');
