4236ea58479c08b417d2fa49a5fce37a4cc4ada9
play.js <git@gituser.com>
2020-12-24 13:36:48 -0800

    debug004 prints out parsed parameters

--- hooks.js
+++ hooks.js
@@ -1,8 +1,8 @@
 function Hold()
 {
   //window.alert("in Hold");
-  document.getElementById("debug").innerHTML="Executing Hold";
-  /*
+  document.getElementById("debug").innerHTML+=" Executing Hold";
+  
   let latlon=document.getElementById("latlon").value;
   window.alert(latlon);
   let legs=document.getElementById("legs").value;
@@ -12,5 +12,5 @@ function Hold()
   let lon=latlon.split(",")[1];
   let params="lat="+lat+" lon="+lon+" legs="+legs+" length="+length+" loops="+loops);
   document.getElementById("result").innerHTML=params;
-  */
+  
 }
